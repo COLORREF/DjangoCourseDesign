cb111fb6654ef9c6569036f4b9fa9abe5a734ebb
BFEMCC <a1840025658@outlook.com>
2024-04-16 10:18:06 +0800

    完善

--- login/models.py
+++ login/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Admin(models.Model):
     """管理员"""
-    user_name = models.CharField(verbose_name="用户名", max_length=32, blank=False, null=False)
+    user_name = models.CharField(verbose_name="用户名", max_length=32, unique=True, blank=False, null=False)
     password = models.CharField(verbose_name="密码", max_length=128, blank=False, null=False)
 
     def __str__(self):
