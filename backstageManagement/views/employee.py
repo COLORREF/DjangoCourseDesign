b9b5ad84c097e60b75911353d875cf21a4395219
COLORREF <a1840025658@outlook.com>
2024-05-22 05:09:03 +0800

    update

--- backstageManagement/views/employee.py
+++ backstageManagement/views/employee.py
@@ -1,21 +1,14 @@
 from django import forms
-from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
+from django.utils.safestring import mark_safe
+
 from backstageManagement import models
 
 
 class EmployeeForm(forms.ModelForm):
     class Meta:
         model = models.Employee
-        # fields = ['name',
-        #           'age',
-        #           'gender',
-        #           'password',
-        #           'account',
-        #           'create_time',
-        #           'department']
         fields = "__all__"
-        # exclude = ['account']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -25,20 +18,87 @@ class EmployeeForm(forms.ModelForm):
 
 
 def employee_management(request):
-    all_user_info: QuerySet = models.Employee.objects.all()
-    # for info in all_user_info:
-    #     print(
-    #         info.id,
-    #         info.name,
-    #         info.age,
-    #         info.password,
-    #         info.account,
-    #         info.create_time.strftime("%Y-%m-%d"),  # 年月日时间
-    #         info.department.industry_title,  # 部门名称
-    #         info.get_gender_display()  # 性别get_字段名_display
-    #     )
+
+    # 搜索处理
+    # all_info = models.PrettyNum.objects.all()  # .order_by("-level")  # order_by排序，-号从高到低，按级别排序
+    data_dict = {}
+    search_value = request.GET.get('search_value')
+    if search_value:  # 如果不为为空
+        data_dict['name__contains'] = search_value  # 字段名__contains 包含某个值
+
+    # 根据页码计算出数据条数的起始位置和结束位置
+    page = int(request.GET.get('page', '1'))  # 当前页
+    page_size = 10  # 每一页的数据条数
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    # 添加页码标签
+
+    # 计算页码，生成标签
+    count = models.Employee.objects.filter(**data_dict).count()  # 数据总条数
+    # 计算页码总数
+    total_page_count, div = divmod(count, page_size)
+    if div != 0:  # 如果有余数总页数要加1
+        total_page_count += 1
+
+    # 计算起始页码和结束页码为当前页的前后5页
+    plus = 5
+    if total_page_count <= 2 * plus + 1:  # 数据总量小于11（前后5页+选中1页）
+        start_page = 1
+        end_page = total_page_count + 1
+    else:  # 数据量大于11
+        if page <= plus:  # 当前选中的是前前5页，正常显示1-10页
+            start_page = 1
+            end_page = 2 * plus + 1
+        else:  # 当前选中页大于5，显示前5页和后5页
+            if (page + plus) > total_page_count:  # 如果选中的是最后几页，即当前选中页+5大于总页数
+                start_page = total_page_count - 2 * plus  # 开始页为最后一页往前11个
+                end_page = total_page_count + 1  # 最后页就是总页数
+            else:
+                start_page = page - plus
+                end_page = page + plus + 1
+
+    # 遍历生成页码
+    page_str_list = []
+
+    # 首页
+    prev = '<li><a href="?page=1">首页</a></li>'
+    page_str_list.append(prev)
+
+    # 上一页
+    if page > 1:
+        prev = '<li><a href="?page={}">上一页</a></li>'.format(page - 1)
+    else:
+        prev = '<li><a href="#">上一页</a></li>'
+    page_str_list.append(prev)
+
+    for i in range(start_page, end_page):
+        # 添加选中样式
+        if i == page:
+            label = '<li class="active"><a href="?page={}">{}</a></li>'.format(i, i)
+        else:
+            label = '<li><a href="?page={}">{}</a></li>'.format(i, i)
+        page_str_list.append(label)
+
+    # 下一页
+    if page < total_page_count:
+        prev = '<li><a href="?page={}">下一页</a></li>'.format(page + 1)
+    else:
+        prev = '<li><a href="#">下一页</a></li>'
+    page_str_list.append(prev)
+
+    # 尾页
+    prev = '<li><a href="?page={}">尾页</a></li>'.format(total_page_count)
+    page_str_list.append(prev)
+
+    page_str = mark_safe("".join(page_str_list))
+
+    all_user_info = models.Employee.objects.filter(**data_dict)[start_index:end_index]  # 取前十条数据
+
     form = EmployeeForm()
-    return render(request, '员工管理.html', {'all_user_info': all_user_info, 'form': form})
+    return render(request,
+                  '员工管理.html',
+                  {"all_user_info": all_user_info, "form": form, "page_str": page_str})
 
 
 def add_employee(request):
