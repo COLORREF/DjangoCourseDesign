c55036e0c3e8c614ceb98c3b6ee6c08504b4a8a4
COLORREF <114940465+COLORREF@users.noreply.github.com>
2024-08-16 10:50:36 +0800

    对errorDict函数的类型注解进行了修改，使其兼容Python 3.9及更早版本，通过动态定义类型来支持不同版本的Python环境

--- login/utils/errorDict.py
+++ login/utils/errorDict.py
@@ -1,17 +1,25 @@
-from django.forms import ModelForm, Form
-
-
-def errorDict(form: ModelForm | Form, fields_errors: dict = None):
-    errors = dict()
-
-    # 字典不为空，说明没有自定义错误，使用默认错误信息
-    if fields_errors:
-        for field, error in fields_errors.items():
-            # form.add_error(field=field, error=error)  # 不再额外给form添加错误信息，直接生成错误信息字典返回
-            errors[field] = error
-        return errors
-
-    # 字典为空，使用默认的错误信息
-    for key, value in form.errors.items():
-        errors[key] = value[0][:-1]  # 删去结尾句号
+import sys  
+from django import forms  
+from typing import Union, Dict, TYPE_CHECKING  
+  
+if sys.version_info >= (3, 10):  
+    # Python 3.10 或更高版本，使用原生的联合操作符  
+    ModelFormOrForm = forms.ModelForm | forms.Form  
+else:  
+    # Python 3.9 或更低版本，使用 typing.Union  
+    ModelFormOrForm = Union[forms.ModelForm, forms.Form]  
+  
+def errorDict(form: ModelFormOrForm, fields_errors: Dict[str, str] = None):  
+    errors = dict()  
+  
+    # 字典不为空，说明没有自定义错误，使用默认错误信息  
+    if fields_errors:  
+        for field, error in fields_errors.items():  
+            # form.add_error(field=field, error=error)  # 不再额外给form添加错误信息，直接生成错误信息字典返回  
+            errors[field] = error  
+        return errors  
+  
+    # 字典为空，使用默认的错误信息  
+    for key, value in form.errors.items():  
+        errors[key] = value[0][:-1]  # 删去结尾句号  
     return errors
